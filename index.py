8c0a7342b5fd90240d595fcf7e1a0c151c908b5e
Rachel Joyce <rachelrj@umich.edu>
2023-12-03 12:05:47 -0700

    Headwater scrape (#8)

--- index.py
+++ index.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.options import Options
 import os
@@ -13,21 +12,34 @@ def run_scrapes():
     # After I added this time.sleep(300), it started working. 
     # Comment out if testing.
     # TODO: Fix this. Avoid the need to sleep.
-    time.sleep(300)
+    time.sleep(200)
+
+    # Use locally to connect to hub
+    # Use on AWS container to connect to hub
+    # hub_url = "http://localhost:4444/wd/hub"
+
+    # Use within docker local container to connect to hub
+    hub_url = "http://selenium_hub:4444/wd/hub"
+    shared_folder = "/shared-data/"
 
     options = webdriver.FirefoxOptions()
+
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-extensions')
 
-    # Use locally to connect to hub
-    # Use on AWS container to connect to hub
-    hub_url = "http://localhost:4444/wd/hub"
+    firefox_profile = webdriver.FirefoxProfile()
+    firefox_profile.set_preference("browser.download.folderList", 2)
+    firefox_profile.set_preference("browser.download.manager.showWhenStarting", False)
+    firefox_profile.set_preference("browser.download.dir", shared_folder)
+    firefox_profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/pdf")
 
-    # Use within docker local container to connect to hub
-    # hub_url = "http://selenium_hub:4444/wd/hub"
+    options.set_preference("firefox_profile", firefox_profile.encoded)
 
-    driver = webdriver.Remote(command_executor=hub_url, options=options)
+    driver = webdriver.Remote(
+        command_executor=hub_url,
+        options=options
+    )
 
     scripts_directory = "scripts"
 
